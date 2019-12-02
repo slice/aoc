d455bd52bdbcbe8f4fbb2dfa1704dd6f0bc63f60
slice <ryaneft@gmail.com>
2019-12-02 00:06:54 -0800

    d2: add

---
+++ src/main/scala/aoc/y2019/Day2.scala
@@ -0,0 +1,87 @@
+package aoc.y2019
+
+import aoc._
+import aoc.util._
+
+import cats.data.State
+
+object Day2 extends Day {
+  val code: List[Int] =
+    slurp("inputs/day2.txt")
+      .strip
+      .split(",")
+      .map(_.toInt)
+      .to(List)
+
+  type Memory = List[Int]
+  case class Brain(memory: Memory, ip: Int, halted: Boolean = false)
+  type Computer = State[Brain, Unit]
+
+  def generateMemory(noun: Int, verb: Int): Memory =
+    code.head :: noun :: verb :: code.drop(3)
+
+  val part1Code = generateMemory(12, 2)
+
+  val state: Computer = State { case original @ Brain(memory, ip, halted) =>
+    // TODO: this is incredibly messy
+
+    if (halted) {
+      (original, ())
+    } else {
+      def binaryOp(f: (Int, Int) => Int): Memory =
+        memory.drop(ip + 1 /* skip opcode */) match {
+          case leftAddress :: rightAddress :: targetAddress :: _ =>
+            (memory.lift(leftAddress), memory.lift(rightAddress)) match {
+              case (Some(left), Some(right)) =>
+                memory.updated(targetAddress, f(left, right))
+              case _ => memory
+            }
+          case _ => memory
+        }
+
+      if (memory(ip) == 99) {
+        (Brain(memory, ip, halted = true), ())
+      } else {
+        val newMemory = memory(ip) match {
+          case 1 => binaryOp(_ + _)
+          case 2 => binaryOp(_ * _)
+        }
+
+        (Brain(newMemory, ip + 4), ())
+      }
+    }
+  }
+
+  def eval(initialMemory: Memory): Int = {
+    // TODO: don't be effectful. is repeatedly calling `runS` the way to go?
+
+    var s = state.runS(Brain(initialMemory, 0))
+
+    while (!s.value.halted) {
+      s = state.runS(s.value)
+    }
+
+    s.value.memory.head
+  }
+
+  def part1: Int = eval(part1Code)
+
+  def part2: Option[Int] = {
+    // you know what ... let's just brute force this
+    //
+    // TODO: find a way to do this without effects
+
+    var result: Option[Int] = None
+
+    for {
+      noun <- 0 to 99
+      verb <- 0 to 99
+    } {
+      if (eval(generateMemory(noun, verb)) == 19690720) {
+        result = Some(100 * noun + verb)
+      }
+    }
+
+    result
+  }
+}
